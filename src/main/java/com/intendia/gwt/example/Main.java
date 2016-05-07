0b06760aeb7cccf300dfc4f1349369297420a42d
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2016-05-07 21:06:29 +0200

    Add Main class to configure a headless DevMode

---
+++ src/main/java/com/intendia/gwt/example/Main.java
@@ -0,0 +1,27 @@
+package com.intendia.gwt.example;
+
+import com.google.gwt.dev.DevMode;
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Thread.currentThread().setContextClassLoader(new URLClassLoader(new URL[] {
+                new File("src/main/java").toURI().toURL(),
+                new File("target/generated-sources/annotations").toURI().toURL() },
+                Thread.currentThread().getContextClassLoader()));
+        DevMode devMode = new DevMode() {
+            {
+                setHeadless(true);
+                new DevMode.ArgProcessor(options).processArgs(
+                        "-superDevMode",
+                        "-war", "target/war",
+                        "-startupUrl", "example/index.html",
+                        "com.intendia.gwt.example.Example");
+            }
+        };
+        devMode.run();
+        System.exit(0);
+    }
+}
