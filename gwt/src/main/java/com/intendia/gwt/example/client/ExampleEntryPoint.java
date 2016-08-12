ff8088c2e2dd73aac5c33629090f13d2ee4799bc
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2016-08-12 10:23:12 +0200

    Fix last snapshot rename of RequestResourceVisitor to RequestResourceBuilder

--- gwt/src/main/java/com/intendia/gwt/example/client/ExampleEntryPoint.java
+++ gwt/src/main/java/com/intendia/gwt/example/client/ExampleEntryPoint.java
@@ -3,7 +3,7 @@ package com.intendia.gwt.example.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.intendia.gwt.autorest.client.RequestResourceVisitor;
+import com.intendia.gwt.autorest.client.RequestResourceBuilder;
 import com.intendia.gwt.autorest.client.ResourceVisitor;
 
 public class ExampleEntryPoint implements EntryPoint {
@@ -14,6 +14,6 @@ public class ExampleEntryPoint implements EntryPoint {
                 "[" + (int) (n.importance * 10.) + "] " + n.display_name + " (" + n.lon + "," + n.lat + ")")));
     }
 
-    static ResourceVisitor osm() { return new RequestResourceVisitor().path("http://nominatim.openstreetmap.org/"); }
+    static ResourceVisitor osm() { return new RequestResourceBuilder().path("http://nominatim.openstreetmap.org/"); }
 
 }
